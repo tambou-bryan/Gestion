6e5170758ee5250f3569d6f6e61c4baf68a118fb
tambou-bryan <tamboubryan@gmail.com>
2021-12-07 21:49:07 +0100

    Initial commit

---
+++ App1/App1/views/login.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace App1.views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class login : ContentPage
+    {
+        public login()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Clicked(object sender, EventArgs e)
+        {
+            if(TxtUsername.Text=="admin" && TxtPassword.Text=="1234")
+            {
+                Navigation.PushAsync(new MyFlyoutPage());
+            }
+            else
+            {
+                DisplayAlert("Opps", "Username or Password is incorrect", "Ok");
+            }
+        }
+
+        private void TapGestureRecognizer_Tapped(object sender, EventArgs e)
+        {
+            Navigation.PushAsync(new RegisterPage());
+        }
+    }
+}
